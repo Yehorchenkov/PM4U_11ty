447e549bb2bfadb412a474787226731cb3b5747a
Oleksii Yehorchenkov <alexee@ukr.net>
2024-01-28 09:36:42 +0100

    add search, navbar

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ const Image = require("@11ty/eleventy-img");
 
 const faviconsPlugin = require("eleventy-plugin-gen-favicons");
 
+// search
+const { execSync } = require('child_process');
+
 module.exports = function(eleventyConfig) {
     // Copy the `css` directory to the output
     eleventyConfig.addPassthroughCopy("./src/styles");
@@ -19,6 +22,32 @@ module.exports = function(eleventyConfig) {
 
     // favicon plugin
     eleventyConfig.addPlugin(faviconsPlugin, {});
+
+    // Image shortcode
+    eleventyConfig.addShortcode("image", async function(src, alt, width, sizes) {
+      const widths = width ? [width, width * 2] : [300, 800, "auto"];
+      let metadata = await Image(`./src/images/${src}`, {
+        widths: widths,
+        formats: ["avif", "jpeg"],
+              urlPath: "/images/",
+              outputDir: "_site/images/"
+      });
+
+      let imageAttributes = {
+        alt,
+        sizes: "(min-width: 30em) 50vw, 100vw",
+        loading: "lazy",
+        decoding: "async",
+      };
+
+      // You bet we throw an error on a missing alt (alt="" works okay)
+      return Image.generateHTML(metadata, imageAttributes);
+    });
+
+    //pagefind indexing after site building
+    eleventyConfig.on('eleventy.after', () => {
+        execSync(`npx pagefind --site _site`, { encoding: 'utf-8' })
+    });
     
     // Return your Object options:
     return {
